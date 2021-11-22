1b52bd2b850f98bebd47baff7c6830d1bc4c21bf
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2021-11-22 00:09:39 -0500

    Added name box

--- main.py
+++ main.py
@@ -12,9 +12,23 @@ root = Tk()
 root.title("Glucometer App")
 root.geometry("500x600")
 
+#Add title to the GUI window
 title = Label(root, text= "Glucometer App")
 titleFont = tkFont.Font(family='Times New Roman', size=30, weight="bold", slant="italic", underline=True)
 title.configure(font=titleFont, fg="black")
 title.pack()
+
+#Current time display on the text-file and name box
+nameLabel = Label(root, text="\nEnter your name")
+nameLabel.pack()
+nameBox = Entry(root)
+name_get = nameBox.get()
+print(name_get)
+nameBox.pack()
+
+current_time = datetime.now()
+in_Mols = float()
+
+
 
 root.mainloop()
