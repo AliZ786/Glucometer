c2d2724f0257b059d317c83b7caf8855914c2607
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2021-11-22 00:44:47 -0500

    Basic changes made

--- main.py
+++ main.py
@@ -7,28 +7,54 @@ import numpy as np
 from tabulate import tabulate
 import matplotlib.pyplot as plt
 
-#Initialisation of the GUI window
+# Initialisation of the GUI window
 root = Tk()
 root.title("Glucometer App")
 root.geometry("500x600")
 
-#Add title to the GUI window
-title = Label(root, text= "Glucometer App")
-titleFont = tkFont.Font(family='Times New Roman', size=30, weight="bold", slant="italic", underline=True)
+# Add title to the GUI window
+title = Label(root, text="Glucometer App")
+titleFont = tkFont.Font(family='Times New Roman', size=30,
+                        weight="bold", slant="italic", underline=True)
 title.configure(font=titleFont, fg="black")
 title.pack()
-
-#Current time display on the text-file and name box
 nameLabel = Label(root, text="\nEnter your name")
 nameLabel.pack()
 nameBox = Entry(root)
-name_get = nameBox.get()
-print(name_get)
 nameBox.pack()
 
-current_time = datetime.now()
-in_Mols = float()
+# Current time display on the text-file and name box
+def get_name():
+    
+    name_get = nameBox.get()
+    return name_get
+    
 
 
+myLabel = Label(root, text="\nEnter your sugar level in mmol/L: ")
+myFont = tkFont.Font(family="Times New Roman", size=14, weight="bold")
+myLabel.configure(font=myFont)
+myLabel.configure(fg="blue")
+myLabel.pack()
+input = Entry(root)
+input.pack()
+
+def get_level():
+    get_name()
+    in_Mols = input.get()
+    in_Mg = (float(in_Mols))*18
+    print(in_Mg)
+
+    f = open("file1.txt", "a")
+    f.write("------------ " + f"{nameBox.get()}" + " sugar level's file ---------")
+    f.write(in_Mols)
+    f.write(str(in_Mg))
+
+
+
+
+
+myButton = Button(root, text="Submit", command= get_level, bg="red", fg="white")
+myButton.pack()
 
 root.mainloop()
