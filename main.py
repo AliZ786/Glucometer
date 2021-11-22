0b199a9c0be8001f8b1f839d61a9169362a66a41
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2021-11-21 23:58:56 -0500

    Added title on GUI window

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ root = Tk()
 root.title("Glucometer App")
 root.geometry("500x600")
 
-
+title = Label(root, text= "Glucometer App")
+titleFont = tkFont.Font(family='Times New Roman', size=30, weight="bold", slant="italic", underline=True)
+title.configure(font=titleFont, fg="black")
+title.pack()
 
 root.mainloop()
